be33ddd9ba2242c106307f36a4aab842305c2229
iomegak12 <jd.ramkumar@gmail.com>
2019-09-06 15:51:38 +0530

    final commit 2

---
+++ MessageSender/Program.cs
@@ -0,0 +1,26 @@
+﻿using MessageSender.Models;
+using MessageSender.Services;
+using System;
+
+namespace MessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MAX_LIMIT = 500;
+            var eventHubInfo = new EventHubInfo
+            {
+                EventHubConnectionString = Environment.GetEnvironmentVariable("EHConnectionString"),
+                EventHubPath = Environment.GetEnvironmentVariable("EHPath"),
+                NoOfMessages = MAX_LIMIT
+            };
+
+            var submitter = new SensorReadingDataSubmitter();
+            var status = submitter.Submit(eventHubInfo).Result;
+
+            Console.WriteLine("Submission Status : " + status);
+            Console.WriteLine("Event Messages have been submitted successfully!");
+        }
+    }
+}
